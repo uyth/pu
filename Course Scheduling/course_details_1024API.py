eb1a536b5509c212e9234955f4d25471a573c1b6
langokalla <martin.langmo.karlstrom@gmail.com>
2017-02-14 13:43:14 +0100

    Images folder

--- Course Scheduling/course_details_1024API.py
+++ Course Scheduling/course_details_1024API.py
@@ -2,7 +2,6 @@
 # Author: Martin Langmo Karlstrøm
 
 import requests
-import json
 
 base_url_1 = "http://www.ntnu.no/web/studier/emner?p_p_id=coursedetailsportlet_WAR_courselistportlet&p_p_lifecycle" \
                 "=2&p_p_resource_id=timetable&_coursedetailsportlet_WAR_courselistportlet_year=2017" \
@@ -11,9 +10,15 @@ base_url_2 = "&year=2017&version=1"
 
 code = input("Course code:\n>> ").upper()
 
+
+
 # Fetch the course
 course = requests.get(base_url_1 + code + base_url_2).json()
 
 # Get relevant data
 # Name
+
+code = course["course"]["summarized"][0]["courseCode"]
 name = course["course"]["summarized"][0]["courseName"]
+print()
+print(code + ' ' + name)
