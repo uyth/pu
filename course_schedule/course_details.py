6574e31fb255ffb2ef3b05147fcd85698f4c50c8
langokalla <martin.langmo.karlstrom@gmail.com>
2017-02-21 13:19:12 +0100

    Readable date fix.

--- course_schedule/course_details.py
+++ course_schedule/course_details.py
@@ -75,7 +75,11 @@ def get_exam_date(code):
     """
     course = json_to_dictionary(code)[0]
     date = course["course"]["assessment"][0]["date"]
+    return date
 
+
+def get_exam_date_readable(code):
+    date = get_exam_date(code)
     # Convert to "readable" date.
     months = {'01': "January", '02': "February", '03':"March", '04': "April", '05': "May", '06': "June", '07': "July",
               '08': "August", '09': "September", '10': "October", '11': "November", '12': "December"}
